99921ae9dda66555650ea28e12310d745dd5dcf4
me@georgenishimura.com <thetmkay>
2015-12-01 14:10:12 +0000

    extrapolate error strings into constants file

---
+++ constants.js
@@ -0,0 +1,8 @@
+module.exports = {
+	errors: {
+		NO_CONFIG_OBJECT:'No config object',
+		NO_CONFIG_USER: 'No user passed in config object',
+		NO_CONFIG_PASSWORD: 'No password passed in config object',
+		NO_CONFIG_BOOK: 'No book id passed in config object'	
+	}
+}
