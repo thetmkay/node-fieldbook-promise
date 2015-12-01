9652591455da53f800739517bec2aff2bb565a10
me@georgenishimura.com <thetmkay>
2015-12-01 14:09:52 +0000

    fixes from test

--- index.js
+++ index.js
@@ -1,85 +1,100 @@
-(function(config){
+module.exports = function(config){
 
 
-	var request = require('superagent-promise')(require('superagent'), this.Promise);
+	var request = require('superagent-promise')(require('superagent'), this.Promise),
+	url = require('url'),
+	constants = require('./constants');	
+
 	var options;
 
-	(function(config){
-		if(!config.user){
-			throw new Error('No user passed in config object');
+	(function(conf){
+		if(!conf) {
+			throw new Error(constants.errors.NO_CONFIG_OBJECT);
+			return;
 		}
 
-		if(!config.password){
-			throw new Error('No password passed in config object');
+		if(!conf.user){
+			throw new Error(constants.errors.NO_CONFIG_USER);
+			return;
 		}
 
-		if(config.book){
-			throw new Error('No defined book in config object');
+		if(!conf.password){
+			throw new Error(constants.errors.NO_CONFIG_PASSWORD);
+			return;
+		}
+
+		if(!conf.book){
+			throw new Error(constants.errors.NO_CONFIG_BOOK);
+			return;
 		}
 
 		options = config;
 	})(config);
 
 
-	function basicRequest() {
-		return request()
-			.accept('json')
-			.auth(this.username, this.password)
+	function execute(req) {
+		return req
+			.set('accept','application/json')
+			.auth(options.user, options.password)
+			.end()
+			.then(function(response){
+				return response.body;
+			}, function(errBody){
+				return Promise.reject(errBody.response.error);
+			});
+		
 	}	
 
 	function buildApi() {
-		var args = Array.prototype.slice.call(arguments); 
-		//put bookId to beginning of array
-		args.unshift(options.book);
-		var path = args.join("/");
+		var path;
+
+		if(!arguments) {
+			path = options.book;			
+		} else {
+			var args = Array.prototype.slice.call(arguments); 
+			//put bookId to beginning of array
+			args.unshift(options.book);
+			path = args.join("/");
+		}
+		
 		return url.resolve("https://api.fieldbook.com/v1/",path);
 	}
 
 	return {
 		getSheets: function(){
 			var api = buildApi();
-			return basicRequest
-				.get(api)
-				.end();
+			return execute(request.get(api));
 		},
 		
 		getSheet: function(sheetId){
 			var api = buildApi(sheetId);
-			return basicRequest()
-				.get(api)
-				.end();
+			return execute(request.get(api));
 		},
 
 		getRecord: function(sheetId, recordId){
 			var api = buildApi(sheetId, recordId);
-			return basicRequest()
-				.get(api)
-				.end();
+			return execute(request.get(api));
 		},
 
 		addRecord: function(sheetId, data){
 			var api = buildApi(sheetId);
-			return basicRequest()
-				.post(api)
-				.send(data)	
-				.end();
+			return execute(request
+							.post(api)
+							.send(data))
 		},
 
 		updateRecord: function(sheetId, recordId, data){
 			var api = buildApi(sheetId, recordId);
-			return basicRequest()
-				.patch(api)
-				.send(data)
-				.end();
+			return execute(request
+							.patch(api)
+							.send(data));
 		},
 
 		deleteRecord: function(sheetId, recordId){
 			var api = buildApi(sheetId, recordId);
-			return basicRequest()
-				.delete(api)
-				.end();
+			return execute(request.del(api));
 		}	
 	}
 
 
-})();
+}
